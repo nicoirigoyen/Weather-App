ca88cb7ab2b68259196a4053ec5c68512586bc5f
nicoirigoyen <nico.irigoyen.terre@hotmail.com>
2021-07-08 15:32:16 -0300

    Updates

---
+++ precache-manifest.1bc53dd5de0f5ce28cabc7da44530586.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ef79c60bc3e2bcee7c6350a86ea88eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9a9d8f634b6904a38c5",
+    "url": "/static/css/main.3a464e96.chunk.css"
+  },
+  {
+    "revision": "6a04e3e489647dea05c4",
+    "url": "/static/js/2.1e70f55f.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/static/js/2.1e70f55f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9a9d8f634b6904a38c5",
+    "url": "/static/js/main.cc889140.chunk.js"
+  },
+  {
+    "revision": "a3deb63255e992068f35",
+    "url": "/static/js/runtime-main.a8223704.js"
+  },
+  {
+    "revision": "75dd2f6fb7787cdb0e62047e2f572333",
+    "url": "/static/media/email.75dd2f6f.png"
+  },
+  {
+    "revision": "59dd1c052ed54ceae8ff11f3ce9d7dbd",
+    "url": "/static/media/fondo2.59dd1c05.jpg"
+  },
+  {
+    "revision": "9672f063ad36dea2d2565aec8f5b5494",
+    "url": "/static/media/logoWeather.9672f063.png"
+  }
+]);
